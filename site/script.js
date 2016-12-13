1c43b6efc161c9075a0cdfc19a7940513852e4e5
paskevich <artempaskevich@gmail.com>
2016-12-13 09:28:03 +0300

    add 1 more bonus bug fixes

--- site/script.js
+++ site/script.js
@@ -3,7 +3,8 @@ function onClick(){
     crs = parseInt(document.getElementById('crs_num').innerHTML);
     std = parseInt(document.getElementById('std_num').innerHTML);
     cdr = parseInt(document.getElementById('cdr_num').innerHTML);
-    c = c + 1 + 2*crs + 10*std + 100*cdr;
+    clk = parseInt(document.getElementById('clk_num').innerHTML);
+    c = c + 1 + 2*crs + 10*std + 100*cdr + 1000*clk;
     document.getElementById('cnt').innerHTML = c;
     if(c>=1000000){
         /*alert("NOT BAD, MAN! NOT BAD! AGAIN!");
@@ -44,6 +45,17 @@ function buyCdr(){
         tmp++;
         document.getElementById('cdr_num').innerHTML = tmp;
         c-=1000;
+        document.getElementById('cnt').innerHTML = c;
+    }
+}
+
+function buyClk(){
+    tmp = document.getElementById('clk_num').innerHTML;
+    c = document.getElementById('cnt').innerHTML;
+    if(c>=10000){
+        tmp++;
+        document.getElementById('clk_num').innerHTML = tmp;
+        c-=10000;
         document.getElementById('cnt').innerHTML = c;
     }
 }
