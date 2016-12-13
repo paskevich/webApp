f9287a009e5730e4c73639db79a2ed3430a89634
paskevich <artempaskevich@gmail.com>
2016-12-13 04:03:34 +0300

    Clicker

--- site/script.js
+++ site/script.js
@@ -46,3 +46,15 @@ function buyCdr(){
         document.getElementById('cnt').innerHTML = c;
     }
 }
+
+function init()
+{
+    sec = 0;
+    setInterval(tick, 1000);
+}
+            
+function tick()
+{
+    sec++;
+    document.getElementById("timer").childNodes[0].nodeValue = sec + " seconds";
+}
