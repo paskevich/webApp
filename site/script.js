d3c4ad99b67420c5c141740e421f58d2c53cb339
paskevich <artempaskevich@gmail.com>
2016-12-13 03:03:44 +0300

    Clicker

---
+++ site/script.js
@@ -0,0 +1,48 @@
+function onClick(){
+    c = parseInt(document.getElementById('cnt').innerHTML);
+    crs = parseInt(document.getElementById('crs_num').innerHTML);
+    std = parseInt(document.getElementById('std_num').innerHTML);
+    cdr = parseInt(document.getElementById('cdr_num').innerHTML);
+    c = c + 1 + 2*crs + 10*std + 100*cdr;
+    document.getElementById('cnt').innerHTML = c;
+    if(c>=1000000){
+        alert("NOT BAD, MAN! NOT BAD! AGAIN!");
+        document.getElementById('cnt').innerHTML = 0;
+        document.getElementById('crs_num').innerHTML = 0;
+        document.getElementById('std_num').innerHTML = 0;
+        document.getElementById('cdr_num').innerHTML = 0;
+    }
+}
+
+function buyCrs(){
+    tmp = document.getElementById('crs_num').innerHTML;
+    c = document.getElementById('cnt').innerHTML;
+    if(c>=10){
+        tmp++;
+        document.getElementById('crs_num').innerHTML = tmp;
+        c-=10;
+        document.getElementById('cnt').innerHTML = c;
+    }
+}
+
+function buyStd(){
+    tmp = document.getElementById('std_num').innerHTML;
+    c = document.getElementById('cnt').innerHTML;
+    if(c>=100){
+        tmp++;
+        document.getElementById('std_num').innerHTML = tmp;
+        c-=100;
+        document.getElementById('cnt').innerHTML = c;
+    }
+}
+
+function buyCdr(){
+    tmp = document.getElementById('cdr_num').innerHTML;
+    c = document.getElementById('cnt').innerHTML;
+    if(c>=1000){
+        tmp++;
+        document.getElementById('cdr_num').innerHTML = tmp;
+        c-=1000;
+        document.getElementById('cnt').innerHTML = c;
+    }
+}
